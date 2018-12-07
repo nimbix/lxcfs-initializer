7e2cdc301b93dcb72bc21511dd1d957e72097089
Paul Schroeder <paul.schroeder@nimbix.net>
2018-12-07 16:00:00 -0600

    Fix use of HOST_LXCFS_DIR..

--- lxcfs-image/start.sh
+++ lxcfs-image/start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+[ -z "$HOST_LXCFS_DIR" ] && HOST_LXCFS_DIR=/var/lib/lxcfs
+
 # Cleanup
-nsenter -m/proc/1/ns/mnt fusermount -u /var/lib/lxcfs 2> /dev/null || true
+nsenter -m/proc/1/ns/mnt fusermount -u $HOST_LXCFS_DIR 2> /dev/null || true
 nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
-        sed -i "/^lxcfs \/var\/lib\/lxcfs fuse.lxcfs/d" /etc/mtab
+        sed -i "|^lxcfs $HOST_LXCFS_DIR fuse.lxcfs|d" /etc/mtab
 
 # Prepare
-[ -z "$HOST_LXCFS_DIR" ] && HOST_LXCFS_DIR=/var/lib/lxcfs
 mkdir -p /host/$HOST_LXCFS_DIR
 mkdir -p /host/usr/local/lib/lxcfs
 cp -f /lxcfs/lxcfs /host/usr/local/bin/lxcfs
 cp -f /lxcfs/liblxcfs.so /host/usr/local/lib/lxcfs/liblxcfs.so
 
 # Mount
-exec nsenter -m/proc/1/ns/mnt lxcfs /var/lib/lxcfs/
+exec nsenter -m/proc/1/ns/mnt lxcfs $HOST_LXCFS_DIR/
