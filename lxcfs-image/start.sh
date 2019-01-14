8926e604cfb1fede77763f356c93261160d5a1c3
Paul Schroeder <paul.schroeder@nimbix.net>
2019-01-14 13:26:15 -0600

    Make lxcfs binary static..

--- lxcfs-image/start.sh
+++ lxcfs-image/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 [ -z "$HOST_LXCFS_DIR" ] && HOST_LXCFS_DIR=/var/lib/lxcfs
+[ -z "$HOST_LXCFS_BIN" ] && HOST_LXCFS_BIN=/usr/local/bin/lxcfs-static
 
 # Cleanup
 nsenter -m/proc/1/ns/mnt fusermount -u $HOST_LXCFS_DIR 2> /dev/null || true
@@ -8,10 +9,9 @@ nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
         sed -i "|^lxcfs $HOST_LXCFS_DIR fuse.lxcfs|d" /etc/mtab
 
 # Prepare
-mkdir -p /host/$HOST_LXCFS_DIR
-mkdir -p /host/usr/local/lib/lxcfs
-cp -f /lxcfs/lxcfs /host/usr/local/bin/lxcfs
-cp -f /lxcfs/liblxcfs.so /host/usr/local/lib/lxcfs/liblxcfs.so
+mkdir -p /host$HOST_LXCFS_DIR
+mkdir -p /host$(dirname $HOST_LXCFS_BIN)
+cp -f /lxcfs/lxcfs /host$HOST_LXCFS_BIN
 
 # Mount
-exec nsenter -m/proc/1/ns/mnt lxcfs $HOST_LXCFS_DIR/
+exec nsenter -m/proc/1/ns/mnt $HOST_LXCFS_BIN $HOST_LXCFS_DIR/
