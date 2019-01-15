e7104d382819826936eaa495a16160d2f6c97b76
Paul Schroeder <paul.schroeder@nimbix.net>
2019-01-15 10:17:22 -0600

    Also make fusermount.static..

--- lxcfs-image/start.sh
+++ lxcfs-image/start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 [ -z "$HOST_LXCFS_DIR" ] && HOST_LXCFS_DIR=/var/lib/lxcfs
-[ -z "$HOST_LXCFS_BIN" ] && HOST_LXCFS_BIN=/usr/local/bin/lxcfs-static
-
-# Cleanup
-nsenter -m/proc/1/ns/mnt fusermount -u $HOST_LXCFS_DIR 2> /dev/null || true
-nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
-        sed -i "|^lxcfs $HOST_LXCFS_DIR fuse.lxcfs|d" /etc/mtab
+[ -z "$HOST_LXCFS_BIN" ] && HOST_LXCFS_BIN=/usr/local/bin/lxcfs.static
+[ -z "$HOST_FUSERMOUNT_BIN" ] && HOST_FUSERMOUNT_BIN=/usr/local/bin/fusermount.static
 
 # Prepare
 mkdir -p /host$HOST_LXCFS_DIR
 mkdir -p /host$(dirname $HOST_LXCFS_BIN)
 cp -f /lxcfs/lxcfs /host$HOST_LXCFS_BIN
+mkdir -p /host$(dirname $HOST_FUSERMOUNT_BIN)
+cp -f /lxcfs/fusermount /host$HOST_FUSERMOUNT_BIN
+
+# Cleanup
+nsenter -m/proc/1/ns/mnt $HOST_FUSERMOUNT_BIN -u $HOST_LXCFS_DIR 2> /dev/null || true
+nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
+        sed -i "|^lxcfs $HOST_LXCFS_DIR fuse.lxcfs|d" /etc/mtab
 
 # Mount
 exec nsenter -m/proc/1/ns/mnt $HOST_LXCFS_BIN $HOST_LXCFS_DIR/
