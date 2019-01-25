0c623a1951c8a86fab5e63bd01447257385efbe9
Paul B Schroeder <paul.schroeder@nimbix.net>
2019-01-25 22:14:51 +0000

    LXCFS is not really set up to be built statically..  Just package and install the .so libs..

--- lxcfs-image/start.sh
+++ lxcfs-image/start.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 [ -z "$HOST_LXCFS_DIR" ] && HOST_LXCFS_DIR=/var/lib/lxcfs
-[ -z "$HOST_LXCFS_BIN" ] && HOST_LXCFS_BIN=/usr/local/bin/lxcfs.static
-[ -z "$HOST_FUSERMOUNT_BIN" ] && HOST_FUSERMOUNT_BIN=/usr/local/bin/fusermount.static
+[ -z "$HOST_LXCFS_INSTALL_DIR" ] && \
+    HOST_LXCFS_INSTALL_DIR=/usr/local/lxcfs-daemonset
 
-# Prepare
-mkdir -p /host$HOST_LXCFS_DIR
-mkdir -p /host$(dirname $HOST_LXCFS_BIN)
-cp -f /lxcfs/lxcfs /host$HOST_LXCFS_BIN
-mkdir -p /host$(dirname $HOST_FUSERMOUNT_BIN)
-cp -f /lxcfs/fusermount /host$HOST_FUSERMOUNT_BIN
+HOST_LXCFS_BIN_DIR=$HOST_LXCFS_INSTALL_DIR/bin
+HOST_LXCFS_LIB_DIR=$HOST_LXCFS_INSTALL_DIR/lib/lxcfs
+
+# Install lxcfs binaries and libs
+mkdir -p /host$HOST_LXCFS_BIN_DIR /host$HOST_LXCFS_LIB_DIR
+cp -f /lxcfs/bin/* /host$HOST_LXCFS_BIN_DIR
+cp -fP /lxcfs/lib/lxcfs/* /host$HOST_LXCFS_LIB_DIR
 
 # Cleanup
-nsenter -m/proc/1/ns/mnt $HOST_FUSERMOUNT_BIN -u $HOST_LXCFS_DIR 2> /dev/null || true
+nsenter -m/proc/1/ns/mnt /bin/sh -c "export LD_LIBRARY_PATH=$HOST_LXCFS_LIB_DIR; $HOST_LXCFS_BIN_DIR/fusermount -u $HOST_LXCFS_DIR" 2> /dev/null || true
 nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
         sed -i "|^lxcfs $HOST_LXCFS_DIR fuse.lxcfs|d" /etc/mtab
 
 # Mount
-exec nsenter -m/proc/1/ns/mnt $HOST_LXCFS_BIN $HOST_LXCFS_DIR/
+exec nsenter -m/proc/1/ns/mnt /bin/sh -c "export LD_LIBRARY_PATH=$HOST_LXCFS_LIB_DIR; $HOST_LXCFS_BIN_DIR/lxcfs $HOST_LXCFS_DIR/"
+
